7ff82d6460b860909643fd965879bc7abe17d8a8
RickK213 <rkippert@att.net>
2017-11-15 13:39:16 -0600

    Finished demo functionality. Assignment complete.

---
+++ ClockRadioDemo/ClockRadioDemo/ClockRadio.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClockRadioDemo
+{
+    class ClockRadio
+    {
+        string time = "4:20PM";
+        bool isAlarmSet;
+        string currentRadioStation;
+
+        public void SetAlarm()
+        {
+            isAlarmSet = true;
+        }
+        public void ChangeRadioStation(string newStation)
+        {
+            currentRadioStation = newStation;
+        }
+    }
+}
